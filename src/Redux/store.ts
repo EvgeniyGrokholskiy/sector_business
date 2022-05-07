4997554fecc77ecb0ddae455842f665f4805a718
EvgeniyGrokholskiy <grokholskiy@gmail.com>
2022-05-07 05:52:08 +0500

    added roboto font, pasted explain why i use redux-toolkit

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,6 +1,12 @@
+import thunk from "redux-thunk";
 import {appReducer} from "./appSlice";
 import {tableReducer} from "./tableSlice";
-import {configureStore} from "@reduxjs/toolkit";
+import {applyMiddleware, combineReducers, configureStore, createStore} from "@reduxjs/toolkit";
+
+/*const rootReducer = combineReducers({
+    app: appReducer,
+    table: tableReducer
+})*/
 
 
 export const store = configureStore({
@@ -9,6 +15,11 @@ export const store = configureStore({
         table: tableReducer
     },
 });
+
+//export const store = createStore(rootReducer, applyMiddleware(thunk)) //Deprecated symbol used, consult docs for better alternative
+// We recommend using the configureStore method of the @reduxjs/toolkit package, which replaces createStore.
+// Redux Toolkit is our recommended approach for writing Redux logic today, including store setup, reducers, data fetching, and more.
+// For more details, please read this Redux docs page: https://redux.js.org/introduction/why-rtk-is-redux-today
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
